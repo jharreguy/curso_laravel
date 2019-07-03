05c896889a71fea510b62ab248df14d978b1bda0
jharreguy <juanmaharreguy@hotmail.com>
2019-07-02 21:30:02 -0300

    seguridad paginas con login

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -5,6 +5,7 @@
 	<link href="{{ asset('bootstrap/css/bootstrap.css') }}" rel="stylesheet">
 </head>
 <body>
+	<div id="app">
 	<div class="row">
 		<div class="col-md-12">
 		<nav class="navbar navbar-default">
@@ -28,7 +29,40 @@
 	      </button>
 	      <a class="navbar-brand" href="#">Brand</a>
 	    </div>
+	    <ul class="navbar-nav ml-auto">
+	    	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    
+        	</div>    
+        	</ul>        
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      {{Form::open(['route' => 'categorias.b', 'method' => 'POST'])}}
@@ -44,7 +78,10 @@
 	</div>
   </div>
 	@yield('contenido')
+</div>
 </body>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
 <script src="{{ asset('bootstrap/js/bootstrap.js') }}">
 </script>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,29 @@
 */
 
 //CRUD
-Route::resource('categorias', 'CategoriasController')->except('show');
-Route::get('/categorias/borradas', 'CategoriasController@borradas')->name('categorias.borradas');
+Route::resource('categorias', 'CategoriasController')->except('show')->middleware('auth');
+Route::get('/categorias/borradas', 'CategoriasController@borradas')->name('categorias.borradas')->middleware('auth');
 Route::get('/categorias/restaurar/{id}', [
         'uses' => 'CategoriasController@restaurar',
         'as'   => 'categorias.restaurar',
-    ]);
-Route::get('/categorias/galeria', 'CategoriasController@galeria')->name('categorias.galeria');
+    ])->middleware('auth');
+Route::get('/categorias/galeria', 'CategoriasController@galeria')->name('categorias.galeria')->middleware('auth');
 
 Route::delete('categorias/{id}/eliminar', [
         'uses' => 'CategoriasController@eliminar',
         'as'   => 'categorias.eliminar',
-    ]);
+    ])->middleware('auth');
 Route::post('categorias/busqueda', [
 	'uses' => 'CategoriasController@busqueda',
         'as'   => 'categorias.b',
-]);
+])->middleware('auth');
 
 //ABM para productos
 Route::resource('productos', 'ProductosController')->except('show');
 Route::delete('productos/{id}/eliminar', [
         'uses' => 'ProductosController@eliminar',
         'as'   => 'productos.eliminar',
-    ]);
+    ])->middleware('auth');
 
 Route::get('/', function () {
 
